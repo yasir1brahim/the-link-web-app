c32270487de0a296dd4faed9e022f492a5c34a9d
mohancarnera <mohan@getcarnera.com>
2022-03-06 19:21:02 +0530

    HTMl & CSS for Top Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.scss';
+import 'font-awesome/css/font-awesome.min.css'; 
 import Authentication from './components/Authentication/Authentication';
 import NavbarTop from './components/shared/NavbarTop/NavbarTop';
 
 function App() {
   return (
     <div className="the-link">
-      <Authentication />
-      {/* <NavbarTop /> */}
+      {/* <Authentication /> */}
+      <NavbarTop />
     </div>
   );
 }
