f41d0180b28fc93f46fe3f6ef61912af93a06aa2
pranshul@getcarnera.com <pranshul@getcarnera.com>
2022-03-13 00:36:03 +0530

    Added axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
-  // headers: { 'X-Access-Token': localStorage.getItem('token') },
+  headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
 });
 
 export default axiosInstance;
