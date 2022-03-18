39a9f9ac500b5e5fd4178e4708c9053b3694d8d1
pranshul@getcarnera.com <pranshul@getcarnera.com>
2022-03-18 12:52:59 +0530

    commented unwanted code

--- src/components/shared/Pagination/Pagination.jsx
+++ src/components/shared/Pagination/Pagination.jsx
@@ -1,25 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
 import ReactPaginate from 'react-paginate';
 
 // Example items, to simulate fetching from another resources.
 const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-function Items({ currentItems }) {
-  return (
-    <>
-      {currentItems &&
-        currentItems.map((item) => (
-          <div>
-            <h3>Item #{item}</h3>
-          </div>
-        ))}
-    </>
-  );
-}
+// function Items({ currentItems }) {
+//   return (
+//     <>
+//       {currentItems &&
+//         currentItems.map((item) => (
+//           <div>
+//             <h3>Item #{item}</h3>
+//           </div>
+//         ))}
+//     </>
+//   );
+// }
 
 function PaginatedItems({ itemsPerPage }) {
   // We start with an empty list of items.
-  const [currentItems, setCurrentItems] = useState(null);
+  // const [currentItems, setCurrentItems] = useState(null);
   const [pageCount, setPageCount] = useState(0);
   // Here we use item offsets; we could also use page offsets
   // following the API or data you're working with.
@@ -43,7 +42,7 @@ function PaginatedItems({ itemsPerPage }) {
   };
 
   return (
-    <div className='pagination-content'>
+    <div className="pagination-content">
       {/* <Items currentItems={currentItems} /> */}
       <ReactPaginate
         breakLabel="..."
@@ -54,9 +53,9 @@ function PaginatedItems({ itemsPerPage }) {
         previousLabel="< previous"
         renderOnZeroPageCount={null}
       />
-      <label className='showing-content d-flex align-items-center justify-content-center'>
+      <label className="showing-content d-flex align-items-center justify-content-center">
         <span> Show</span>
-        <select className='pagination-select'>
+        <select className="pagination-select">
           <option>5</option>
           <option>10</option>
           <option>25</option>
