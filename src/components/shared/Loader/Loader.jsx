954e2a2b4845ee46de311c0e3120e6e4a998c52a
pranshul@getcarnera.com <pranshul@getcarnera.com>
2022-05-14 15:40:34 +0530

    Reduced loader animation time and added logic to close loader

--- src/components/shared/Loader/Loader.jsx
+++ src/components/shared/Loader/Loader.jsx
@@ -2,23 +2,21 @@ import React from 'react';
 
 const Loader = (props) => {
   return (
-    <div
-      className={
-        'loader-wrapper ' +
-        (props.showComponentLoader ? 'wrapper-component' : '')
-      }
-    >
-      <div class="dots">
-        <div class="dot1">
-        </div>
-        <div class="dot2">
-        </div>
-        <div class="dot3">
-        </div>
-        <div class="dot4">
+    props.showComponentLoader && (
+      <div
+        className={
+          'loader-wrapper ' +
+          (props.showComponentLoader ? 'wrapper-component' : '')
+        }
+      >
+        <div class="dots">
+          <div class="dot1"></div>
+          <div class="dot2"></div>
+          <div class="dot3"></div>
+          <div class="dot4"></div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
