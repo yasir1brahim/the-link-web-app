d7e2b6d1769173ff8471a2a3ce18cc1fccc80417
Pranshul-Carnera <PranshulCarnera@123>
2022-06-05 11:04:10 +0530

    Added link in axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  // baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: 'https://log-manager-api-prod.thelink.ai',
   // headers: {
   //   Authorization: `Bearer ${localStorage.getItem('token')}`,
   // },
