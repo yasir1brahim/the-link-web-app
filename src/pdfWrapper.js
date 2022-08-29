cdcf57a2969bc9f63dfa2dc2f6dbbff6c4c956d8
Pranshul-Carnera <PranshulCarnera@123>
2022-08-29 23:01:47 +0530

    PDF Annotation Tools

--- src/pdfWrapper.js
+++ src/pdfWrapper.js
@@ -1,7 +1,6 @@
 import PdfReader from './components/PdfReader'
 // import { useLocation } from 'react-router-dom';
 
-
 import React from 'react'
 const PdfWrapper = (props) => {
   // const location = useLocation()
@@ -9,7 +8,7 @@ const PdfWrapper = (props) => {
 
     <div className='ss-pdf-wrraper'>
       {/* <PdfReader url={`https://linkdocs.s3.amazonaws.com/original/project_10_05_5000_-_METAL_FABRICATIONS.pdf`}/> */}
-      <PdfReader url={props.pdfData.url} textLoc={props.pdfData.textLoc} />
+      <PdfReader url={props.pdfData.url} textLoc={props.pdfData.textLoc} docId={props.pdfData.docId}/>
       {/* <PdfReader
         url={new URLSearchParams(location.search).get('url') || ''}
         textLoc={JSON.parse(new URLSearchParams(location.search).get('textLoc').replaceAll("'", '"'))}
