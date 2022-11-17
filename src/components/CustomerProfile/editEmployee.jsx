dd910926e8c2ffa8b574f7b0589648fc350fa099
Priyanshi Tiwari <priyanshitiwari@Priyanshis-MacBook-Pro.local>
2022-11-17 18:41:41 +0530

    Fixed warnings

--- src/components/CustomerProfile/editEmployee.jsx
+++ src/components/CustomerProfile/editEmployee.jsx
@@ -1,4 +1,4 @@
-import React, { useState , useRef} from 'react';
+import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import axiosInstance from '../../config/axios';
 import { toast } from 'react-toastify';
