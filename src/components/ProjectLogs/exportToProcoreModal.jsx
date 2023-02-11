cf1c0e0bc32107edacfecf5d819ab59b14e24887
Pranshul-Carnera <PranshulCarnera@123>
2023-02-11 23:07:41 +0530

    handled all  selected rows condition

--- src/components/ProjectLogs/exportToProcoreModal.jsx
+++ src/components/ProjectLogs/exportToProcoreModal.jsx
@@ -45,14 +45,14 @@ const ExportToProcoreModal = (props) => {
   }, [props?.companyId]);
 
   const handleExportToProcore = async () => {
-    const selectedRows = localStorage.getItem('selectedRows')?.split(',')?.map( row => JSON.parse(row));
+    const selectedRows = localStorage.getItem('selectedRows') === '' ? "All" : localStorage.getItem('selectedRows')?.split(',')?.map( row => JSON.parse(row));
     try {
       await axiosInstance({
         method: "post",
         url: "/procore/create_submittals",
         data: {
           project_id: Number(projectId),
-          records: selectedRows && selectedRows?.length > 0 ? selectedRows : "All", // array of ids
+          records: selectedRows, // array of ids
           status_id: get(selectStatus,`${[0]}.value`),
         },
       }).then((resp)=>{
