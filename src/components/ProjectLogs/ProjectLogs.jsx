6672860471f87c108347c3153a1c7e51fadecee5
Pranshul-Carnera <sid@getcarnera.com>
2023-03-28 00:01:24 +0530

    changes for testing theee procore modal

--- src/components/ProjectLogs/ProjectLogs.jsx
+++ src/components/ProjectLogs/ProjectLogs.jsx
@@ -257,7 +257,9 @@ const ProjectLogs = () => {
   };
   
     if (authCode) {
-      if (procoreProjectMappingsAPICalled && projectMappingNoContent) {
+      if (procoreProjectMappingsAPICalled
+        //  && projectMappingNoContent
+         ) {
         const fetchData = async () => {
           setProcoreModal(true);
           const companyResp = await axiosInstance({
@@ -269,11 +271,12 @@ const ProjectLogs = () => {
         fetchData().catch((error) => {
           handleError(error);
         });
-      } else if (procoreProjectMappingsAPICalled) {
-        searchParams.set("code", "");
-        // setNavigateToSubmittal(!navigateToSubmittal);
-        handleExportToProcore();
-      }
+      } 
+      // else if (procoreProjectMappingsAPICalled) {
+      //   searchParams.set("code", "");
+      //   // setNavigateToSubmittal(!navigateToSubmittal);
+      //   handleExportToProcore();
+      // }
     }
   }, [projectMappingNoContent, searchParams, authCode, procoreProjectMappingsAPICalled, projectId, navigate, customerId, logType, selectedRows, companyId]);
 
