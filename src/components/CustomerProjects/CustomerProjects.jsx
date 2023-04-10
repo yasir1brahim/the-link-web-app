b1e021c0567aa5154a5e40b1ad0a32324efa501c
Pranshul-Carnera <PranshulCarnera@123>
2023-04-10 08:48:58 +0530

    Made Grid view as the default view in project listing

--- src/components/CustomerProjects/CustomerProjects.jsx
+++ src/components/CustomerProjects/CustomerProjects.jsx
@@ -18,31 +18,43 @@ import CreateEmployee from '../CustomerProfile/createEmployee';
 import moment from 'moment';
 import handleError from '../../config/errorHandler';
 
-const CustomerProjects = ({toggleSlider, slider, customerData,setCustomerData, pageRefresh, setPageRefresh, toggleCreateProjectModal, createProjectModal, projectData ,setProjectData, handleLaunch}) => {
+const CustomerProjects = ({
+  toggleSlider,
+  slider,
+  customerData,
+  setCustomerData,
+  pageRefresh,
+  setPageRefresh,
+  toggleCreateProjectModal,
+  createProjectModal,
+  projectData,
+  setProjectData,
+  handleLaunch,
+  isArchived,
+  toggleArchive,
+}) => {
   const [employeeModal, setEmployeeModal] = useState(false);
   const [editModal, setEditModal] = useState(false);
-  const [isArchived, toggleArchive] = useState(false);
-
   const toggleEditModal = () => setEditModal(!editModal);
   const toggleEmployeeModal = () => setEmployeeModal(!employeeModal);
 
   const [project, setProject] = useState({});
-  
+
   const [currentItems, setCurrentItems] = useState([]);
   const [itemsPerPage, setItemsPerPage] = useState(5);
   const [isLoading, setLoading] = useState(false);
   const { state } = useLocation();
   // const customer = state;
-  const roleId = localStorage.getItem('roleId')
+  const roleId = localStorage.getItem("roleId");
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       const response = await axiosInstance({
-        method: 'get',
+        method: "get",
         url: state?.customer_id
           ? `/customer/${state.customer_id}`
-          : `/customer/${localStorage.getItem('userId')}`,
+          : `/customer/${localStorage.getItem("userId")}`,
       });
       setLoading(false);
       setCustomerData(response.data.message[0]);
@@ -50,33 +62,9 @@ const CustomerProjects = ({toggleSlider, slider, customerData,setCustomerData, p
     };
 
     fetchData().catch((error) => {
-      handleError(error)
+      handleError(error);
     });
   }, [state, pageRefresh, setCustomerData]);
-
-  useEffect(() => {
-    const url = roleId === '6' || roleId === '7' ?
-      `/emp/projects/${localStorage.getItem('userId')}` :
-      state?.customer_id
-        ? `/projects/${state.customer_id}`
-        : `/projects/${localStorage.getItem('userId')}`
-    const fetchData = async () => {
-      const response = await axiosInstance({
-        method: 'get',
-        url,
-      });
-      // setProjectData(response.data.message);
-      setProjectData(
-        isArchived ? response.data.archived_projects : response.data.message
-      );
-      console.log(response.data.message);
-    };
-
-    fetchData().catch((error) => {
-      handleError(error)
-    });
-  }, [state, pageRefresh, isArchived, roleId, setProjectData]);
-
 
   const handleEdit = (project) => {
     setProject(project);
@@ -88,23 +76,26 @@ const CustomerProjects = ({toggleSlider, slider, customerData,setCustomerData, p
     if (!errors) {
       try {
         const response = await axiosInstance({
-          method: 'put',
-          url: '/updateProject',
+          method: "put",
+          url: "/updateProject",
           data: {
             project_name: project.project_name,
             lead_contact: project.lead_contact,
             start_date: project?.start_date
               ? moment(
-                new Date((project?.start_date).replaceAll('-', '/'))
-              ).format('YYYY-MM-DD')
-              : '',
+                  new Date((project?.start_date).replaceAll("-", "/"))
+                ).format("YYYY-MM-DD")
+              : "",
             end_date: project?.end_date
               ? moment(
-                new Date((project?.end_date).replaceAll('-', '/'))
-              ).format('YYYY-MM-DD')
-              : '',
-            customer_id: localStorage.getItem('roleId') === '0' ? state.customer_id : localStorage.getItem('userId'),
-            status: isArchived ? 'Active' : 'Archived',
+                  new Date((project?.end_date).replaceAll("-", "/"))
+                ).format("YYYY-MM-DD")
+              : "",
+            customer_id:
+              localStorage.getItem("roleId") === "0"
+                ? state.customer_id
+                : localStorage.getItem("userId"),
+            status: isArchived ? "Active" : "Archived",
             project_id: project.project_id,
           },
         });
@@ -114,40 +105,35 @@ const CustomerProjects = ({toggleSlider, slider, customerData,setCustomerData, p
         }
       } catch (error) {
         console.log(error.message);
-        handleError(error)
+        handleError(error);
       }
     }
   };
 
   return (
     <>
-     
-        
-       
-     
-          <div className="customer-projects-content">
-            <div className="customer-project-details">
-              {/* when there are Zero Users */}
-              {/* <a className='noprojects-wrapper d-flex align-items-center justify-content-center w-100' href='javascript:void(0);'>
+      <div className="customer-projects-content">
+        <div className="customer-project-details">
+          {/* when there are Zero Users */}
+          {/* <a className='noprojects-wrapper d-flex align-items-center justify-content-center w-100' href='javascript:void(0);'>
                             <span className='d-flex align-items-center justify-content-center'><AddUser /> Create Users/Employees, then Add a Project</span>
                         </a> */}
-              <div className="table-top-content">
-                <div className="table-heading">
-                  
-                  <label className="table-entries">
-                    Showing entries{' '}
-                    <span className="showing-strong">
-                      {currentItems.length}
-                    </span>{' '}
-                    of{' '}
-                    <span className="showing-strong">{projectData.length}</span>
-                    .
-                  </label>
-                </div>
-                
-                <div className="grid-list-toggle" >
-                  <div className="table-bulk-changes" style={{ marginRight: '30px' }}>
-                    {roleId !== '6' && roleId !== '7' && <div style={{ marginLeft: '10px' }}>
+          <div className="table-top-content">
+            <div className="table-heading">
+              <label className="table-entries">
+                Showing entries{" "}
+                <span className="showing-strong">{currentItems.length}</span> of{" "}
+                <span className="showing-strong">{projectData.length}</span>.
+              </label>
+            </div>
+
+            <div className="grid-list-toggle">
+              <div
+                className="table-bulk-changes"
+                style={{ marginRight: "30px" }}
+              >
+                {roleId !== "6" && roleId !== "7" && (
+                  <div style={{ marginLeft: "10px" }}>
                     <button
                       type="button"
                       className="btn btn-secondary btn-sm"
@@ -155,36 +141,38 @@ const CustomerProjects = ({toggleSlider, slider, customerData,setCustomerData, p
                     >
                       + Add Employee
                     </button>
-                    </div>}
-
-                    {roleId !== '6' && roleId !== '7' && 
-                      <button
-                        onClick={() => toggleArchive(!isArchived)}
-                        type="button"
-                        className="btn btn-secondary btn-sm"
-                      >
-                        {isArchived ? 'View Active' : 'View Archived'}
-                      </button>}
                   </div>
+                )}
 
-                  <span className="tag-list-view" >List View</span>
-                  <div className="gl-toggle-wrapper">
-                    <label class="switch">
-                      <input type="checkbox" checked={slider}/>
-                      <span class="slider round" onClick={()=>toggleSlider(!slider)}></span>
-                    </label>
-                  </div>
-                  <span className="tag-list-view">Grid View</span>
-                </div>
-              
-                
-                
+                {roleId !== "6" && roleId !== "7" && (
+                  <button
+                    onClick={() => toggleArchive(!isArchived)}
+                    type="button"
+                    className="btn btn-secondary btn-sm"
+                  >
+                    {isArchived ? "View Active" : "View Archived"}
+                  </button>
+                )}
               </div>
-              <div className="l-table-wrapper">
-                <table className="table">
-                  <thead>
-                    <tr>
-                      {/* <th className="ticket-checkbox">
+
+              <span className="tag-list-view">List View</span>
+              <div className="gl-toggle-wrapper">
+                <label class="switch">
+                  <input type="checkbox" checked={slider} />
+                  <span
+                    class="slider round"
+                    onClick={() => toggleSlider(!slider)}
+                  ></span>
+                </label>
+              </div>
+              <span className="tag-list-view">Grid View</span>
+            </div>
+          </div>
+          <div className="l-table-wrapper">
+            <table className="table">
+              <thead>
+                <tr>
+                  {/* <th className="ticket-checkbox">
                         <div className="form-group">
                           <div className="custom-control custom-checkbox">
                             <input
@@ -200,45 +188,45 @@ const CustomerProjects = ({toggleSlider, slider, customerData,setCustomerData, p
                           </div>
                         </div>
                       </th> */}
-                      <th>
-                        <span>
-                          Existing Projects <i className=""></i>
-                        </span>
-                      </th>
-                      <th>
-                        <span>
-                          Status<i className="sort-d"></i>
-                        </span>
-                      </th>
-                      <th>
-                        <span>
-                          Lead Contact<i className="sort-i"></i>
-                        </span>
-                      </th>
-                      <th>
-                        <span>
-                          Users<i className="sort-i"></i>
-                        </span>
-                      </th>
-                      <th>
-                        <span>
-                          Start Date<i className="sort-d"></i>
-                        </span>
-                      </th>
-                      <th>
-                        <span>
-                          End Date<i className="sort-d"></i>
-                        </span>
-                      </th>
-                      <th>Action</th>
-                    </tr>
-                  </thead>
-                  {project ?
-                    <tbody>
-                      {currentItems.map((project) => {
-                        return (
-                          <tr>
-                            {/* <td className="ticket-checkbox">
+                  <th>
+                    <span>
+                      Existing Projects <i className=""></i>
+                    </span>
+                  </th>
+                  <th>
+                    <span>
+                      Status<i className="sort-d"></i>
+                    </span>
+                  </th>
+                  <th>
+                    <span>
+                      Lead Contact<i className="sort-i"></i>
+                    </span>
+                  </th>
+                  <th>
+                    <span>
+                      Users<i className="sort-i"></i>
+                    </span>
+                  </th>
+                  <th>
+                    <span>
+                      Start Date<i className="sort-d"></i>
+                    </span>
+                  </th>
+                  <th>
+                    <span>
+                      End Date<i className="sort-d"></i>
+                    </span>
+                  </th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              {project ? (
+                <tbody>
+                  {currentItems.map((project) => {
+                    return (
+                      <tr>
+                        {/* <td className="ticket-checkbox">
                               <div className="form-group">
                                 <div className="custom-control custom-checkbox">
                                   <input
@@ -254,84 +242,93 @@ const CustomerProjects = ({toggleSlider, slider, customerData,setCustomerData, p
                                 </div>
                               </div>
                             </td> */}
-                            <td>{project.project_name}</td>
-                            <td>{project.status}</td>
-                            <td>{project.lead_contact}</td>
-                            <td>{project.users}</td>
-                            <td>{project.start_date}</td>
-                            <td>{project.end_date}</td>
-                            <td>
-                              <div className="action-wrapper">
+                        <td>{project.project_name}</td>
+                        <td>{project.status}</td>
+                        <td>{project.lead_contact}</td>
+                        <td>{project.users}</td>
+                        <td>{project.start_date}</td>
+                        <td>{project.end_date}</td>
+                        <td>
+                          <div className="action-wrapper">
+                            <button
+                              type="button"
+                              className="btn btn-secondary btn-sm"
+                              onClick={() => handleLaunch(project)}
+                            >
+                              Launch
+                            </button>
+                            {roleId !== "6" && roleId !== "7" && (
+                              <>
                                 <button
-                                  type="button"
-                                  className="btn btn-secondary btn-sm"
-                                  onClick={() => handleLaunch(project)}
-                                >
-                                  Launch
-                                </button>
-                                {roleId !== '6' && roleId !== '7' && <><button
                                   type="button"
                                   className="btn btn-secondary btn-sm"
                                   onClick={() => handleEdit(project)}
                                 >
                                   Edit
                                 </button>
-                                  <button
-                                    type="button"
-                                    className="btn btn-secondary btn-sm"
-                                    onClick={() => handleArchiveProject(project)}
-                                    disabled={isArchived && localStorage.getItem('roleId') !== '0'}
-                                  >
-                                    {!isArchived ? 'Archive' : 'Unarchive'}
-                                  </button></>}
-                              </div>
-                            </td>
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                    :
-                    <tbody>
-                      <tr>
-                        <td className='text-center' colSpan={7}>No data</td>
+                                <button
+                                  type="button"
+                                  className="btn btn-secondary btn-sm"
+                                  onClick={() => handleArchiveProject(project)}
+                                  disabled={
+                                    isArchived &&
+                                    localStorage.getItem("roleId") !== "0"
+                                  }
+                                >
+                                  {!isArchived ? "Archive" : "Unarchive"}
+                                </button>
+                              </>
+                            )}
+                          </div>
+                        </td>
                       </tr>
-                    </tbody>
-                  }
-                </table>
-              </div>
-              <div className="table-footer-content">
-                <PaginatedItems
-                  items={projectData}
-                  setCurrentItems={setCurrentItems}
-                  itemsPerPage={itemsPerPage}
-                  setItemsPerPage={setItemsPerPage}
-                />
-              </div>
-              <CreateEmployee
-                modal={employeeModal}
-                toggleModal={toggleEmployeeModal}
-                customer={state || customerData}
-                pageRefresh={pageRefresh}
-                setPageRefresh={setPageRefresh}
-              />
-              <CreateProject
-                modal={createProjectModal}
-                toggleModal={toggleCreateProjectModal}
-                customer={state || customerData}
-                pageRefresh={pageRefresh}
-                setPageRefresh={setPageRefresh}
-              />
-              <EditProject
-                modal={editModal}
-                toggleModal={toggleEditModal}
-                customer={state || customerData}
-                project={project}
-                pageRefresh={pageRefresh}
-                setPageRefresh={setPageRefresh}
-              />
-            </div>
+                    );
+                  })}
+                </tbody>
+              ) : (
+                <tbody>
+                  <tr>
+                    <td className="text-center" colSpan={7}>
+                      No data
+                    </td>
+                  </tr>
+                </tbody>
+              )}
+            </table>
           </div>
-          <Loader showComponentLoader={isLoading} />
+          <div className="table-footer-content">
+            <PaginatedItems
+              items={projectData}
+              setCurrentItems={setCurrentItems}
+              itemsPerPage={itemsPerPage}
+              setItemsPerPage={setItemsPerPage}
+            />
+          </div>
+          <CreateEmployee
+            modal={employeeModal}
+            toggleModal={toggleEmployeeModal}
+            customer={state || customerData}
+            pageRefresh={pageRefresh}
+            setPageRefresh={setPageRefresh}
+          />
+          <CreateProject
+            modal={createProjectModal}
+            toggleModal={toggleCreateProjectModal}
+            customer={state || customerData}
+            pageRefresh={pageRefresh}
+            setPageRefresh={setPageRefresh}
+          />
+          <EditProject
+            modal={editModal}
+            toggleModal={toggleEditModal}
+            customer={state || customerData}
+            project={project}
+            pageRefresh={pageRefresh}
+            setPageRefresh={setPageRefresh}
+          />
+        </div>
+      </div>
+      <Loader showComponentLoader={isLoading} />
 
       <ToastContainer
         position="bottom-center"
