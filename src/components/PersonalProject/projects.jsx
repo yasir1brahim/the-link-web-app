dfd81014eae89de9dd33c056ae8a527f411fddb8
Pranshul-Carnera <PranshulCarnera@123>
2023-04-21 06:23:43 +0530

    Redirect to logs screen after upload

--- src/components/PersonalProject/projects.jsx
+++ src/components/PersonalProject/projects.jsx
@@ -31,6 +31,12 @@ const Projects = () => {
     setCreateProjectModal(!createProjectModal);
   const toggleUploadSpecsModal = () => setUploadSpecsModal(!uploadSpecsModal);
 
+  useEffect(() => {
+    if (!uploadSpecsModal) {
+      setPdfFile({});
+    }
+  }, [uploadSpecsModal]);
+
   // function related to upload specs
   const backToUpload = () => {
     toggleErrorModal(false);
@@ -67,10 +73,10 @@ const Projects = () => {
   };
 
   // function used in the project tiles to navigate to project details
+  const custId = localStorage.getItem("roleId") === "0"
+  ? state.customer_id
+  : localStorage.getItem("userId")
   const handleLaunch = (project, qaDashboard) => {
-    const custId = localStorage.getItem("roleId") === "0"
-    ? state.customer_id
-    : localStorage.getItem("userId")
     // project?.project_type === "ufgs"
       // ?
        navigate(`/project-logs?projectDetails=${project?.project_id},${custId},Classified`, {
@@ -181,6 +187,8 @@ const Projects = () => {
           successModal={successModal}
           toggleSuccessModal={toggleSuccessModal}
           fileData={fileData}
+          logScreenUrl={`/project-logs?projectDetails=${specUploadProject?.project_id},${custId},Classified`}
+          project={specUploadProject}
         />
       </div>
     </>
