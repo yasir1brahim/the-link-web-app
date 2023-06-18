9127f5ec7046e0b93450e386b823ff0a345d35de
Pranshul-Carnera <sid@getcarnera.com>
2023-06-19 00:11:03 +0530

    removed: logic to add cols

--- src/components/ProjectLogs/ProjectLogs.jsx
+++ src/components/ProjectLogs/ProjectLogs.jsx
@@ -519,10 +519,10 @@ const ProjectLogs = () => {
   // add the selected rows in session storage to be used by export procore
   useEffect(() => {
     // do not update the values if navigated from procore page
-    if (document.referrer && selected?.length) {
+    // if (document.referrer && selected?.length) {
       const rowsSelected = selected.toString();
       sessionStorage.setItem("selectedRows", `${rowsSelected}`);
-    }
+    // }
   }, [selected]);
 
   // const downloadExcel = (logs, fileName) => {
