023546e2cffa664fe05d6ba30cff0ac17d6c4a46
Pranshul-Carnera <sid@getcarnera.com>
2023-06-19 00:37:59 +0530

    updated: session to local storage

--- src/components/ProjectLogs/ProjectLogs.jsx
+++ src/components/ProjectLogs/ProjectLogs.jsx
@@ -521,7 +521,7 @@ const ProjectLogs = () => {
     // do not update the values if navigated from procore page
     // if (document.referrer && selected?.length) {
       const rowsSelected = JSON.stringify(selected);
-      sessionStorage.setItem("selectedRows", `${rowsSelected}`);
+      localStorage.setItem("selectedRows", `${rowsSelected}`);
     // }
   }, [selected]);
 
