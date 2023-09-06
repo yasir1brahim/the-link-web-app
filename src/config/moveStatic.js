339ce861779c18962f3ee69ef8f9c8889c0ea3ee
Arjun Sabharwal <141849651+Arjun-Carnera@users.noreply.github.com>
2023-09-06 22:17:13 +0530

    Prod push sep v3 (#345)
    * updated package.json to reflict build script
    
    * build script changes

--- src/config/moveStatic.js
+++ src/config/moveStatic.js
@@ -4,6 +4,7 @@
 
 const fs = require('fs-extra');
 const path = require('path');
+var chmodr = require('chmodr');
 
 const src = path.join(
   __dirname,
@@ -17,4 +18,11 @@ fs.copy(src, dest, function (err) {
     return console.error(err);
   }
   console.log('Copy completed!');
+  chmodr(dest, 0o700, (err) => {
+    if (err) {
+      console.log('Failed to execute chmod', err);
+    } else {
+      console.log('Successfully executed chmod  + 700');
+    }
+  });
 });
