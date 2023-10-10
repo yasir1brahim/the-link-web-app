47b9f65546313935b74013f0e2953fa8e17c11fb
Pranshul-Carnera <PranshulCarnera@123>
2023-10-10 14:51:25 +0530

    Clear selection on click of clear

--- src/components/ProjectLogs/ProjectLogs.jsx
+++ src/components/ProjectLogs/ProjectLogs.jsx
@@ -103,7 +103,6 @@ const ProjectLogs = () => {
   const [rowsPerPage, setRowsPerPage] = React.useState(25);
   const [page, setPage] = React.useState(1);
 
-
   useEffect(() => {
     if (!modal) {
       setPdfFile({});
@@ -338,7 +337,7 @@ const ProjectLogs = () => {
   };
   const fetchLogData = async (page, itemsPerPage, filters, search) => {
     setLoading(true);
-    setErrorMessage('Fetching data...')
+    setErrorMessage('Fetching data...');
     const response = await axiosInstance({
       method: 'post',
       url: '/filter_logs',
@@ -354,7 +353,7 @@ const ProjectLogs = () => {
     });
     // setLogData(response.data.message);
     setSelectedFilterValue(response.data.all_filter_vals);
-    
+
     const submittalLogs = response.data.message;
     selectedLogData.length
       ? setSelectedLogData(submittalLogs)
@@ -372,16 +371,17 @@ const ProjectLogs = () => {
         // search: search || '',
         // filters: { ...filters },
         order_col: '',
-        order: '',
+        order: ''
       }
     });
     setCompleteLogData(completeResponse.data.message);
-    setErrorMessage('')
+    setErrorMessage('');
     if (response.data.message.length === 0) {
-     if(search){ setErrorMessage('Sorry, no results found for your search query.');} else {
-      setErrorMessage('Please upload documents, before seeing the logs.')
-     }
-
+      if (search) {
+        setErrorMessage('Sorry, no results found for your search query.');
+      } else {
+        setErrorMessage('Please upload documents, before seeing the logs.');
+      }
     }
     setTotalCount(response?.data?.total_count);
   };
@@ -518,7 +518,7 @@ const ProjectLogs = () => {
         toast.success('List created successfully', {
           position: 'bottom-center'
         });
-        setSelected([])
+        setSelected([]);
       } catch (error) {
         handleError(error);
       }
@@ -590,7 +590,10 @@ const ProjectLogs = () => {
                     <button
                       type="button"
                       className="btn btn-primary mr-3 btn-small"
-                      onClick={() => setSelectedLogData([])}
+                      onClick={() => {
+                        setSelectedLogData([]);
+                        setSelected([]);
+                      }}
                     >
                       Clear Selection
                     </button>
