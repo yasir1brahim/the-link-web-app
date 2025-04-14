a2d7372d0da840cf5051e66d7c3f67eeaf7648ff
Avery Pawelek <averycpawelek@gmail.com>
2025-04-14 09:14:20 -0500

    Fixing bug with display

--- src/components/ProjectLogs/combinedLogs.jsx
+++ src/components/ProjectLogs/combinedLogs.jsx
@@ -403,11 +403,13 @@ export default function CombinedLogs(props) {
 
   const formatSpecSection = (specSection) => {
     if (typeof specSection !== "string") return specSection;
-    const fiveOrSixDigitString = specSection.slice(0, 2) + " " + specSection.slice(2, 4) + " " + specSection.slice(4);
+    const fiveOrSixDigitString = specSection.slice(0, 2) + " " + specSection.slice(2, 4);
     if (specSection.length > 6) {
-      return fiveOrSixDigitString + "." + specSection.slice(6);
+      return fiveOrSixDigitString + " " + specSection.slice(4, 6) + "." + specSection.slice(6);
     }
-    return fiveOrSixDigitString;
+    else {
+      return fiveOrSixDigitString + " " + specSection.slice(4);
+    }
   };
 
   const formatSubmittalNumber = (number) => {
