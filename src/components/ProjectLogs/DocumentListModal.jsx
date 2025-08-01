14219435806dbf4ae5ac958dd25a7f57fa6694cc
Yasir Ibrahim <yasir.ibrahim@gmail.com>
2025-08-01 15:01:19 +0500

    (WIP) TBL-524 : Added a new field in the modal on the frontend and need to add a new endpoint in the backend.

--- src/components/ProjectLogs/DocumentListModal.jsx
+++ src/components/ProjectLogs/DocumentListModal.jsx
@@ -1,34 +1,53 @@
 import React from "react";
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from "reactstrap";
+import axiosInstance from "../../config/axios";
+import { toast } from "react-toastify";
 
-const DocumentListModal = ({ isOpen, toggle, documents }) => {
+const DocumentListModal = ({ isOpen, toggle, documents, projectId, projectVersionId }) => {
+
   return (
-    <Modal isOpen={isOpen} toggle={toggle} fade={false} className="new-customer modal-md">
+    <Modal isOpen={isOpen} toggle={toggle} fade={false} className="new-customer modal-lg">
       <ModalHeader toggle={toggle}>Uploaded Documents</ModalHeader>
       <ModalBody>
         {documents?.length ? (
-          <table className="table">
-            <thead>
-              <tr>
-                <th>File Name</th>
-                <th>Date Uploaded</th>
-              </tr>
-            </thead>
-            <tbody>
-              {documents.map((doc) => (
-                <tr key={doc.id}>
-                  <td style={{fontSize: '14px'}}>{doc.document_name}</td>
-                  <td style={{fontSize: '14px'}}>{doc.created_at ? new Date(doc.created_at).toLocaleDateString() : "-"}</td>
+          <div style={{ overflowX: "auto" }}>
+            <table className="table" style={{ minWidth: "600px" }}>
+              <thead>
+                <tr>
+                  <th style={{ width: "60%" }}>File Name</th>
+                  <th style={{ width: "20%" }}>Date Uploaded</th>
+                  <th style={{ width: "20%" }}>Action</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {documents.map((doc) => (
+                  <tr key={doc.id}>
+                    <td style={{ fontSize: "14px" }}>{doc.document_name}</td>
+                    <td style={{ fontSize: "14px" }}>
+                      {doc.created_at ? new Date(doc.created_at).toLocaleDateString() : "-"}
+                    </td>
+                    <td>
+                      <Button
+                        color="primary"
+                        size="sm"
+                        // onClick={() => handleReprocess(doc.id, doc.document_name)}
+                      >
+                        Reprocess
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         ) : (
           <div>No documents uploaded yet.</div>
         )}
       </ModalBody>
       <ModalFooter>
-        <Button color="secondary" onClick={toggle}>Close</Button>
+        <Button color="secondary" onClick={toggle}>
+          Close
+        </Button>
       </ModalFooter>
     </Modal>
   );
