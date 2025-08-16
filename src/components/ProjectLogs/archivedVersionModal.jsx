30444d681793e68e74494946a5ac825520cbc210
Yasir Ibrahim <yasir.ibrahim@gmail.com>
2025-08-16 16:26:15 +0500

    TBL-631: Updated the old Tootips from reactstrap to mui Tooltip

--- src/components/ProjectLogs/archivedVersionModal.jsx
+++ src/components/ProjectLogs/archivedVersionModal.jsx
@@ -1,7 +1,7 @@
 import { React, useState } from "react";
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from "reactstrap";
 import { RestoreIcon } from "../shared/icons/restoreIcon";
-import { Tooltip } from "reactstrap";
+import Tooltip from '@mui/material/Tooltip';
 
 const ArchivedVersionsModal = ({
   isOpen,
@@ -10,8 +10,6 @@ const ArchivedVersionsModal = ({
   onUnarchive,
   loadingUnarchiveId,
 }) => {
-  const [restoreTooltip, setRestoreTooltip] = useState(null);
-  
   return(
     <Modal isOpen={isOpen} toggle={toggle} fade={false} className="modal-md">
       <ModalHeader toggle={toggle}>Archived Versions</ModalHeader>
@@ -31,29 +29,20 @@ const ArchivedVersionsModal = ({
                 <tr key={version.id}>
                   <td>{version.version_name}</td>
                   <td>
-                    <>
+                    <Tooltip 
+                      title={loadingUnarchiveId === version.id ? "Unarchiving..." : "Restore Project"} 
+                      arrow
+                      placement="left"
+                    >
                       <span
                         onClick={() => {
                           onUnarchive(version.id);
                         }}
                         style={{ cursor: "pointer" }}
-                        id={`restore-tooltip-${version.id}`}
                       >
                         <RestoreIcon />
                       </span>
-                      <Tooltip
-                        placement="left"
-                        target={`restore-tooltip-${version.id}`}
-                        isOpen={restoreTooltip === version.id}
-                        toggle={() =>
-                          setRestoreTooltip(
-                            restoreTooltip === version.id ? null : version.id
-                          )
-                        }
-                      >
-                        {loadingUnarchiveId === version.id ? "Unarchiving..." : "Restore Project"}
-                      </Tooltip>
-                    </>
+                    </Tooltip>
                   </td>
                 </tr>
               ))}
